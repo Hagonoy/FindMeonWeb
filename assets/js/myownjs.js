75ae11b5987ada28955aee17e54af2cd236684fc
Hagonoy <rico.jr.macabenlar@gmail.com>
2024-01-25 01:15:47 +0800

    add announcement notif

--- assets/js/myownjs.js
+++ assets/js/myownjs.js
@@ -20,7 +20,8 @@ function topFunction() {
 }
 // show loading screen
 (function(){
-  var myDiv = document.getElementById("loader"),
+  var myDiv = document.getElementById("loader")
+  var closebtn = document.getElementById("sclosebtn")
     show = function(){
       myDiv.style.visibility = "visible";
       document.querySelector("body").style.backgroundColor = "#04AA6D";
@@ -33,7 +34,32 @@ function topFunction() {
       myDiv.style.display = "none";
       document.querySelector("body").style.visibility = "visible";
       document.querySelector("body").style.backgroundColor = "#36454F";
+      // closebtn.style.display = "none";
     };
 
   show();
 })();
+(function(){
+  announcement = function(){
+        var myDivs = document.getElementById("announcement")
+        var p = document.getElementById("announcement-p")
+        var closebtn = document.getElementById("sclosebtn")
+    fetch('https://122987c6-c1f8-41f4-bac4-82d0530ac971.mock.pstmn.io/announcement').then( response => response.json()).then(({announcement,announcementmessage}) =>{
+      if(announcement){
+        myDivs.style.display = "block";
+        p.innerHTML = announcementmessage;
+        closebtn.style.display = "block";
+      }else{
+        closebtn.style.display = "none";
+      }
+    })
+  }
+  announcement();
+})();
+
+function annclose(){
+  var announcementdiv = document.getElementById("announcement");
+  announcementdiv.style.display = "none";
+}
+
+// push notification
