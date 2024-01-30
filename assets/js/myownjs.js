b690167f6b7628707917eb1aba673d6fdfe24192
Hagonoy <rico.jr.macabenlar@gmail.com>
2024-01-30 21:43:27 +0800

    add img carousel

--- assets/js/myownjs.js
+++ assets/js/myownjs.js
@@ -1,29 +1,10 @@
-// top button here
-// Get the button
-let mybutton = document.getElementById("myBtn");
-    
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 // show loading screen
 (function(){
   var myDiv = document.getElementById("loader")
   var closebtn = document.getElementById("sclosebtn")
     show = function(){
-      myDiv.style.visibility = "visible";
+      myDiv.style.display = "visible";
       document.querySelector("body").style.backgroundColor = "#04AA6D";
       document.querySelector("body").style.visibility = "hidden";
 
